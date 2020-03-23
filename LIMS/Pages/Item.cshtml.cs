05d5d1f22a718501147faac8fe67d9412cb4f0d5
Charles Noble <charlesddnoble@gmail.com>
2020-03-23 08:40:54 -0400

    Add Search page. Updated Item/Search pages, they now read the the HTTP GET and then use inputs to query the DB

--- LIMS/Pages/Item.cshtml.cs
+++ LIMS/Pages/Item.cshtml.cs
@@ -10,46 +10,52 @@ namespace LIMS
 {
     public class ItemModel : PageModel
     {
-        public string Title { get; private set; } = "Sample Title of a Book";
-        public string Author { get; private set; } = "John Doe";
-        public string Genre { get; private set; } = "Fantasy";
+        public string Title { get; private set; }
+        public string Author { get; private set; }
+        public string Genre { get; private set; }
         public DateTime DatePublished { get; private set; }
-        public string ISBN { get; private set; } = "0000-0000-00000";
-        public string Summary { get; private set; } = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-        public string ImagePath { get; private set; } = "placeholder-2.png";
-        public string Log { get; private set; } = "";
+        public string ISBN { get; private set; }
+        public string Summary { get; private set; } 
+        public string ImagePath { get; private set; }
+        public string Log { get; private set; }
 
-        public void OnGet()
+        public void OnGet(string isbn)
         {
             var handler = new ConnectionHandler();
             var connection = handler.Connection;
+            if (isbn == null)
+            {
+                ISBN = "0000000000000";
+            } else
+            {
+                ISBN = isbn;
+            }
+
             try
             {
-                Log += "Connecting to MySQL...<br>";
-
-                string sql = "SELECT * FROM Books WHERE ISBN='0000000000000'";
+                string sql = "SELECT * FROM Books WHERE ISBN=@ISBN";
                 MySqlCommand cmd = new MySqlCommand(sql, connection);
+                cmd.Parameters.AddWithValue("@ISBN", ISBN);
                 MySqlDataReader rdr = cmd.ExecuteReader();
-
-                rdr.Read();
-
-                this.ISBN = (string) rdr[0];
-                this.Title = (string) rdr[1];
-                this.Genre = (string) rdr[2];
-                this.Author = (string) rdr[3];
-                this.Summary = (string) rdr[4];
-                this.DatePublished = (DateTime) rdr[5];
-                this.ImagePath = (string)rdr[6];
-
+                Console.WriteLine(ISBN);
+                if (rdr.Read())
+                {
+                    //this.ISBN = (string) rdr[0];
+                    Title = (string)rdr[1];
+                    Genre = (string)rdr[2];
+                    Author = (string)rdr[3];
+                    Summary = (string)rdr[4];
+                    DatePublished = (DateTime)rdr[5];
+                    ImagePath = (string)rdr[6];
+                }
                 rdr.Close();
             }
             catch (Exception ex)
             {
-                Log += ex.ToString()+ "<br>";
+                Console.WriteLine(ex);
             }
 
             connection.Close();
-            Log += "Done.<br>";
         }
     }
 }
---
+++ LIMS/Pages/Search.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySql.Data.MySqlClient;
+
+namespace LIMS
+{
+    public class SearchModel : PageModel
+    {
+        public string Filter { get; private set; }
+        public string Search { get; private set; }
+        public List<Dictionary<string, string>> Results { get; private set; } = new List<Dictionary<string, string>>();
+
+        public void OnGet(string filter, string search)
+        {
+
+            if (filter == "Title" || filter == "Author" || filter == "Search")
+            {
+                Filter = filter;
+            } else
+            {
+                Filter = "Title";
+            }
+
+            if (search == null)
+            {
+                Search = "";
+            }
+            Search = "%" + Search + "%";
+
+            var handler = new ConnectionHandler();
+            var connection = handler.Connection;
+
+            try
+            {
+                string sql = "SELECT imagePath, title, author, genre, summary, ISBN FROM Books WHERE @Filter LIKE @Search";
+                MySqlCommand cmd = new MySqlCommand(sql, connection);
+                cmd.Parameters.AddWithValue("@Filter", Filter);
+                cmd.Parameters.AddWithValue("@Search", Search);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    string summary = (string)rdr[4];
+                    if (summary.Length >= 150)
+                    {
+                        summary = summary.Remove(150, summary.Length-1-150) + "...";
+                    }
+
+                    Dictionary<string, string> row = new Dictionary<string, string>
+                    {
+                        { "imagePath", (string)rdr[0] },
+                        { "title", (string)rdr[1] },
+                        { "author", (string)rdr[2] },
+                        { "genre", (string)rdr[3] },
+                        { "summary", summary },
+                        { "ISBN", (string)rdr[5] }
+                    };
+                    Results.Add(row);
+                    //Console.WriteLine(String.Format("[imagePath:{0},title:{1},author:{2},genre:{3},summary:{4},ISBN:{5}]",
+                                                    //row["imagePath"], row["title"], row["author"], row["genre"], row["summary"], row["ISBN"]));
+                }
+
+                rdr.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            connection.Close();
+        }
+    }
+}
