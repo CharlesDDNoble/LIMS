852f265bc034e1bc1bbacb64526a44bea2161354
Charles Noble <charlesddnoble@gmail.com>
2020-03-24 12:53:12 -0400

    Fixed AJAX for registration on both server and clent-side.

--- LIMS/Pages/Register.cshtml.cs
+++ LIMS/Pages/Register.cshtml.cs
@@ -10,6 +10,7 @@ using MySql.Data.MySqlClient;
 using System.Text;
 using System.Diagnostics;
 using Newtonsoft.Json;
+using System.IO;
 
 namespace LIMS
 {
@@ -20,76 +21,93 @@ namespace LIMS
 
         }
 
-        public JsonResult OnPost(string data)
+        public class RegistrationForm
         {
-            //try
-            //{
-                Console.WriteLine(data);
-                //var handler = new ConnectionHandler();
-                //MySqlTransaction trans = handler.Connection.BeginTransaction();
-                //string sql = "SELECT * FROM Users WHERE username=@USERNAME";
-                //MySqlCommand cmd = new MySqlCommand(sql, trans.Connection);
-                //cmd.Parameters.AddWithValue("@USERNAME", username);
-                //MySqlDataReader rdr = cmd.ExecuteReader();
+            public string firstName { get; set; }
+            public string lastName { get; set; }
+            public string Username { get; set; }
+            public string Password1 { get; set; }
+            public string Password2 { get; set; }
+            public string Address { get; set; }
+            public string City { get; set; }
+            public string State { get; set; }
+            public string Phone { get; set; }
+            public string Zip { get; set; }
+        }
 
-                //// these bools track the current posts errors, as opposed to the bools that are
-                //// past via POST which track the previous request to tell the user what the error was.
-                //var hasNull = false;
-                //var hasDifPass= false;
-                //var isUnameTaken = false;
-                //var hasError = true;
 
-            //    if (firstName == null || lastName == null || username == null || password1 == null || password2 == null
-            //        || address == null || city == null || state == null || zip == null || phone == null)
-            //    {
-            //        hasNull = true;
-            //    }
+        public JsonResult OnPost([FromBody]RegistrationForm form)
+        {
+            try
+            {
+                var handler = new ConnectionHandler();
+                MySqlTransaction trans = handler.Connection.BeginTransaction();
+                string sql = "SELECT * FROM Users WHERE username=@USERNAME";
+                MySqlCommand cmd = new MySqlCommand(sql, trans.Connection);
+                cmd.Parameters.AddWithValue("@USERNAME", form.Username);
+                MySqlDataReader rdr = cmd.ExecuteReader();
 
-            //    if (password1 != password2 && !hasError)
-            //    {
-            //        hasDifPass = true;
-            //    }
+                var hasNull = false;
+                var hasDifPass = false;
+                var isUnameTaken = false;
+                var hasError = true;
 
-            //    if (!rdr.Read() && !hasError)
-            //    {
-            //        Console.WriteLine("Register: No Error in fields");
-            //        // create a random salt to hash the password with to guard against rainbow tables and other site breaches
-            //        var rand = new Random((int)Stopwatch.GetTimestamp());
-            //        SHA256 hash = SHA256.Create();
+                if (form.firstName == null || form.lastName == null || form.Username == null || form.Password1 == null || form.Password2 == null
+                    || form.Address == null || form.City == null || form.State == null || form.Zip == null || form.Phone == null)
+                {
+                    hasNull = true;
+                }
 
-            //        var salt = hash.ComputeHash(Encoding.ASCII.GetBytes(username + rand.Next().ToString()));
-            //        var hashedPassword = hash.ComputeHash(Encoding.ASCII.GetBytes(password1 + salt));
+                if (form.Password1 != form.Password2 && !hasError)
+                {
+                    hasDifPass = true;
+                }
 
-            //        sql = "INSERT INTO Users(username,password,salt,accountType,firstName,lastName,address,city,zip,state,phone) " +
-            //              "VALUES (@USERNAME, @PASSWORD, @SALT, 'guest', @FIRSTNAME, @LASTNAME, @ADDRESS, @CITY, @ZIP, @STATE, @PHONE)";
-            //        cmd = new MySqlCommand(sql, trans.Connection);
-            //        cmd.Parameters.AddWithValue("@USERNAME", username);
-            //        cmd.Parameters.AddWithValue("@PASSWORD", hashedPassword);
-            //        cmd.Parameters.AddWithValue("@SALT", salt);
-            //        cmd.Parameters.AddWithValue("@FIRSTNAME", firstName);
-            //        cmd.Parameters.AddWithValue("@LASTNAME", lastName);
-            //        cmd.Parameters.AddWithValue("@ADDRESS", address);
-            //        cmd.Parameters.AddWithValue("@CITY", city);
-            //        cmd.Parameters.AddWithValue("@ZIP", zip);
-            //        cmd.Parameters.AddWithValue("@STATE", state);
-            //        cmd.Parameters.AddWithValue("@PHONE", phone);
-            //        rdr = cmd.ExecuteReader();
-            //        trans.Commit();
-            //    }
-            //    else
-            //    {
-            //        isUnameTaken = true;
-            //    }
-            //    rdr.Close();
-            //    if (hasError) {
-            //    }
-            //}
-            //catch (Exception ex)
-            //{
-            //    Console.WriteLine(ex);
-            //}
+                if (!rdr.Read() && !hasError)
+                {
+                    Console.WriteLine("Register: No Error in fields");
+                    // create a random salt to hash the password with to guard against rainbow tables and other site breaches
+                    var rand = new Random((int)Stopwatch.GetTimestamp());
+                    SHA256 hash = SHA256.Create();
 
-            return new JsonResult("", new System.Text.Json.JsonSerializerOptions());
+                    var salt = hash.ComputeHash(Encoding.ASCII.GetBytes(form.Username + rand.Next().ToString()));
+                    var hashedPassword = hash.ComputeHash(Encoding.ASCII.GetBytes(form.Password1 + salt));
+
+                    sql = "INSERT INTO Users(username,password,salt,accountType,firstName,lastName,address,city,zip,state,phone) " +
+                          "VALUES (@USERNAME, @PASSWORD, @SALT, 'guest', @FIRSTNAME, @LASTNAME, @ADDRESS, @CITY, @ZIP, @STATE, @PHONE)";
+                    cmd = new MySqlCommand(sql, trans.Connection);
+                    cmd.Parameters.AddWithValue("@USERNAME", form.Username);
+                    cmd.Parameters.AddWithValue("@PASSWORD", hashedPassword);
+                    cmd.Parameters.AddWithValue("@SALT", salt);
+                    cmd.Parameters.AddWithValue("@FIRSTNAME", form.firstName);
+                    cmd.Parameters.AddWithValue("@LASTNAME", form.lastName);
+                    cmd.Parameters.AddWithValue("@ADDRESS", form.Address);
+                    cmd.Parameters.AddWithValue("@CITY", form.City);
+                    cmd.Parameters.AddWithValue("@ZIP", form.Zip);
+                    cmd.Parameters.AddWithValue("@STATE", form.State);
+                    cmd.Parameters.AddWithValue("@PHONE", form.Phone);
+                    rdr = cmd.ExecuteReader();
+                    trans.Commit();
+                }
+                else
+                {
+                    isUnameTaken = true;
+                }
+                rdr.Close();
+                if (hasError)
+                {
+                    return new JsonResult($"{{\"hasNullField\":\"{hasNull}\"," +
+                                          $"\"hasDifferentPasswords\":\"{hasDifPass}\"," +
+                                          $"\"isUsernameTaken\":\"{isUnameTaken}\"," +
+                                          $"\"success\":\"false\"}}", new System.Text.Json.JsonSerializerOptions());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            return new JsonResult("{\"success\":\"true\"}", new System.Text.Json.JsonSerializerOptions());
         }
     }
 }
