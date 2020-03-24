4d5e93dbf805a554468e39a8587b29fea796d2df
Charles Noble <charlesddnoble@gmail.com>
2020-03-24 10:23:18 -0400

    Registration now uses AJAX to send the form to the server.

--- LIMS/Pages/Register.cshtml.cs
+++ LIMS/Pages/Register.cshtml.cs
@@ -9,96 +9,87 @@ using System.Security.Cryptography;
 using MySql.Data.MySqlClient;
 using System.Text;
 using System.Diagnostics;
+using Newtonsoft.Json;
 
 namespace LIMS
 {
     public class RegisterModel : PageModel
     {
-        public bool IsUsernameTaken { get; private set; } = false;
-        public bool HasNullField { get; private set; } = false;
-        public bool HasDifferentPasswords { get; private set; } = false;
-        public bool HasToRollback { get; private set; } = false;
-
-        public void OnPost()
+        public void OnGet()
         {
 
         }
-        public IActionResult OnPost(string firstName, string lastName, string username, string password1,
-                           string password2, string address, string city, string state, string zip,
-                           string phone)
+
+        public JsonResult OnPost(string data)
         {
-            var redirect = Redirect("/Register");
+            //try
+            //{
+                Console.WriteLine(data);
+                //var handler = new ConnectionHandler();
+                //MySqlTransaction trans = handler.Connection.BeginTransaction();
+                //string sql = "SELECT * FROM Users WHERE username=@USERNAME";
+                //MySqlCommand cmd = new MySqlCommand(sql, trans.Connection);
+                //cmd.Parameters.AddWithValue("@USERNAME", username);
+                //MySqlDataReader rdr = cmd.ExecuteReader();
 
-            try
-            {
-                var handler = new ConnectionHandler();
-                MySqlTransaction trans = handler.Connection.BeginTransaction();
-                string sql = "SELECT * FROM Users WHERE username=@USERNAME";
-                MySqlCommand cmd = new MySqlCommand(sql, trans.Connection);
-                cmd.Parameters.AddWithValue("@USERNAME", username);
-                MySqlDataReader rdr = cmd.ExecuteReader();
+                //// these bools track the current posts errors, as opposed to the bools that are
+                //// past via POST which track the previous request to tell the user what the error was.
+                //var hasNull = false;
+                //var hasDifPass= false;
+                //var isUnameTaken = false;
+                //var hasError = true;
 
-                if (firstName == null || lastName == null || username == null || password1 == null || password2 == null
-                    || address == null || city == null || state == null || zip == null || phone == null)
-                {
-                    HasNullField = true;
-                    HasToRollback = true;
-                }
+            //    if (firstName == null || lastName == null || username == null || password1 == null || password2 == null
+            //        || address == null || city == null || state == null || zip == null || phone == null)
+            //    {
+            //        hasNull = true;
+            //    }
 
-                if (password1 != password2 && !HasToRollback)
-                {
-                    HasDifferentPasswords = true;
-                    HasToRollback = true;
-                }
+            //    if (password1 != password2 && !hasError)
+            //    {
+            //        hasDifPass = true;
+            //    }
 
-                if (!rdr.Read() && !HasToRollback)
-                {
-                    // create a random salt to hash the password with to guard against rainbow tables and other site breaches
-                    var rand = new Random((int)Stopwatch.GetTimestamp());
-                    SHA256 hash = SHA256.Create();
+            //    if (!rdr.Read() && !hasError)
+            //    {
+            //        Console.WriteLine("Register: No Error in fields");
+            //        // create a random salt to hash the password with to guard against rainbow tables and other site breaches
+            //        var rand = new Random((int)Stopwatch.GetTimestamp());
+            //        SHA256 hash = SHA256.Create();
 
-                    var salt = hash.ComputeHash(Encoding.ASCII.GetBytes(username + rand.Next().ToString()));
-                    var hashedPassword = hash.ComputeHash(Encoding.ASCII.GetBytes(password1 + salt));
+            //        var salt = hash.ComputeHash(Encoding.ASCII.GetBytes(username + rand.Next().ToString()));
+            //        var hashedPassword = hash.ComputeHash(Encoding.ASCII.GetBytes(password1 + salt));
 
-                    sql = "INSERT INTO Users(username,password,salt,accountType,firstName,lastName,address,city,zip,state,phone) " +
-                          "VALUES (@USERNAME, @PASSWORD, @SALT, 'guest', @FIRSTNAME, @LASTNAME, @ADDRESS, @CITY, @ZIP, @STATE, @PHONE)";
-                    cmd = new MySqlCommand(sql, trans.Connection);
-                    cmd.Parameters.AddWithValue("@USERNAME", username);
-                    cmd.Parameters.AddWithValue("@PASSWORD", hashedPassword);
-                    cmd.Parameters.AddWithValue("@SALT", salt);
-                    cmd.Parameters.AddWithValue("@FIRSTNAME", firstName);
-                    cmd.Parameters.AddWithValue("@LASTNAME", lastName);
-                    cmd.Parameters.AddWithValue("@ADDRESS", address);
-                    cmd.Parameters.AddWithValue("@CITY", city);
-                    cmd.Parameters.AddWithValue("@ZIP", zip);
-                    cmd.Parameters.AddWithValue("@STATE", state);
-                    cmd.Parameters.AddWithValue("@PHONE", phone);
-                    rdr = cmd.ExecuteReader();
-                }
-                else
-                {
-                    IsUsernameTaken = true;
-                    HasToRollback = true;
-                }
-                rdr.Close();
+            //        sql = "INSERT INTO Users(username,password,salt,accountType,firstName,lastName,address,city,zip,state,phone) " +
+            //              "VALUES (@USERNAME, @PASSWORD, @SALT, 'guest', @FIRSTNAME, @LASTNAME, @ADDRESS, @CITY, @ZIP, @STATE, @PHONE)";
+            //        cmd = new MySqlCommand(sql, trans.Connection);
+            //        cmd.Parameters.AddWithValue("@USERNAME", username);
+            //        cmd.Parameters.AddWithValue("@PASSWORD", hashedPassword);
+            //        cmd.Parameters.AddWithValue("@SALT", salt);
+            //        cmd.Parameters.AddWithValue("@FIRSTNAME", firstName);
+            //        cmd.Parameters.AddWithValue("@LASTNAME", lastName);
+            //        cmd.Parameters.AddWithValue("@ADDRESS", address);
+            //        cmd.Parameters.AddWithValue("@CITY", city);
+            //        cmd.Parameters.AddWithValue("@ZIP", zip);
+            //        cmd.Parameters.AddWithValue("@STATE", state);
+            //        cmd.Parameters.AddWithValue("@PHONE", phone);
+            //        rdr = cmd.ExecuteReader();
+            //        trans.Commit();
+            //    }
+            //    else
+            //    {
+            //        isUnameTaken = true;
+            //    }
+            //    rdr.Close();
+            //    if (hasError) {
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex);
+            //}
 
-                if (!HasToRollback)
-                {
-                    trans.Commit();
-                    redirect = Redirect("/Login?success=true");
-                }
-                else
-                {
-                    redirect = Redirect(String.Format("/Register?error=true&hasnullfield={0}&hasdifferentpasswords={1}&isusernametaken={2}",
-                                                                HasNullField, HasDifferentPasswords, IsUsernameTaken));
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-            }
-
-            return redirect;
+            return new JsonResult("", new System.Text.Json.JsonSerializerOptions());
         }
     }
 }
