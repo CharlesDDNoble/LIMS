ef0a9235e18226431164fdb24b0f0fa5f87f903f
Charles Noble <32346153+CharlesDDNoble@users.noreply.github.com>
2020-04-12 22:23:00 -0400

    Update create_lims_db.sql

--- scripts/create_lims_db.sql
+++ scripts/create_lims_db.sql
@@ -1,91 +1,187 @@
-CREATE DATABASE IF NOT EXISTS lims;
-USE lims;
+-- MySQL Workbench Forward Engineering
 
-CREATE TABLE Users (
-    userId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(256) UNIQUE NOT NULL,
-    password VARCHAR(256) NOT NULL,
-    salt VARCHAR(256) NOT NULL,
-    accountType VARCHAR(256) NOT NULL,
-    firstName VARCHAR(256) NOT NULL,
-    lastName VARCHAR(256) NOT NULL,
-    address VARCHAR(256) NOT NULL,
-    city VARCHAR(256) NOT NULL,
-    state CHAR(2) NOT NULL,
-    zip CHAR(5) NOT NULL,
-    phone CHAR(10) NOT NULL
-);
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-CREATE TABLE Books (
-    ISBN CHAR(13) NOT NULL PRIMARY KEY,
-    title VARCHAR(256) NOT NULL,
-    genre VARCHAR(256),
-    author VARCHAR(256),
-    summary VARCHAR(2048),
-    datePublished DATE,
-    imagePath VARCHAR(1024)
-);
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema lims
+-- -----------------------------------------------------
 
-CREATE TABLE BookDetails (
-    bookId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    ISBN CHAR(13) NOT NULL,
-    bookCondition VARCHAR(256),
-    location VARCHAR(256),
-    availability VARCHAR(20),
-    FOREIGN KEY (ISBN) REFERENCES Books(ISBN)
-);
+-- -----------------------------------------------------
+-- Schema lims
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `lims` DEFAULT CHARACTER SET latin1 ;
+USE `lims` ;
 
-CREATE TABLE UserReviews (
-    reviewId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    userId INT NOT NULL,
-    ISBN CHAR(13) NOT NULL,
-    rating INT NOT NULL,
-    reviewText VARCHAR(2048),
-    FOREIGN KEY (userId) REFERENCES Users(userId),
-    FOREIGN KEY (ISBN) REFERENCES Books(ISBN)
-);
+-- -----------------------------------------------------
+-- Table `lims`.`Books`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `lims`.`Books` (
+  `ISBN` CHAR(13) NOT NULL,
+  `title` VARCHAR(256) NOT NULL,
+  `genre` VARCHAR(256) NULL DEFAULT NULL,
+  `author` VARCHAR(256) NULL DEFAULT NULL,
+  `summary` VARCHAR(2048) NULL DEFAULT NULL,
+  `datePublished` DATE NULL DEFAULT NULL,
+  `imagePath` VARCHAR(1024) NULL DEFAULT NULL,
+  PRIMARY KEY (`ISBN`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
 
-CREATE TABLE BookRequests (
-    requestId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    userId INT NOT NULL,
-    ISBN CHAR(13) NOT NULL,
-    dateRequested DATE NOT NULL,
-    FOREIGN KEY (userId) REFERENCES Users(userId),
-    FOREIGN KEY (ISBN) REFERENCES Books(ISBN)
-);
 
-CREATE TABLE BookHistory (
-    bookHistoryId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    userId INT NOT NULL,
-    ISBN CHAR(13) NOT NULL,
-    dateCheckout DATE,
-    dateDue DATE,
-    dateReturned DATE,
-    FOREIGN KEY (userId) REFERENCES Users(userId),
-    FOREIGN KEY (ISBN) REFERENCES Books(ISBN)
-);
+-- -----------------------------------------------------
+-- Table `lims`.`BookDetails`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `lims`.`BookDetails` (
+  `bookId` INT(11) NOT NULL AUTO_INCREMENT,
+  `ISBN` CHAR(13) NOT NULL,
+  `bookCondition` VARCHAR(256) NULL DEFAULT NULL,
+  `location` VARCHAR(256) NULL DEFAULT NULL,
+  `availability` VARCHAR(20) NULL DEFAULT NULL,
+  PRIMARY KEY (`bookId`),
+  INDEX `ISBN` (`ISBN` ASC),
+  CONSTRAINT `BookDetails_ibfk_1`
+    FOREIGN KEY (`ISBN`)
+    REFERENCES `lims`.`Books` (`ISBN`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 9
+DEFAULT CHARACTER SET = latin1;
 
-CREATE TABLE Reservations (
-    reservationId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    userId INT NOT NULL,
-    ISBN CHAR(13) NOT NULL,
-    dateReserved DATE NOT NULL,
-    FOREIGN KEY (userId) REFERENCES Users(userId),
-    FOREIGN KEY (ISBN) REFERENCES Books(ISBN)
-);
 
-CREATE TABLE Orders (
-    orderId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    ISBN CHAR(13) NOT NULL,
-    quantity INT NOT NULL,
-    dateOrdered DATE NOT NULL,
-    dateExpected DATE,
-    dateRecieved DATE,
-    FOREIGN KEY (ISBN) REFERENCES Books(ISBN)
-);
+-- -----------------------------------------------------
+-- Table `lims`.`Users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `lims`.`Users` (
+  `userId` INT(11) NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(256) NOT NULL,
+  `password` VARCHAR(256) NOT NULL,
+  `salt` VARCHAR(256) NOT NULL,
+  `accountType` VARCHAR(256) NOT NULL,
+  `firstName` VARCHAR(256) NOT NULL,
+  `lastName` VARCHAR(256) NOT NULL,
+  `address` VARCHAR(256) NOT NULL,
+  `city` VARCHAR(256) NOT NULL,
+  `state` CHAR(2) NOT NULL,
+  `zip` CHAR(5) NOT NULL,
+  `phone` CHAR(10) NOT NULL,
+  PRIMARY KEY (`userId`),
+  UNIQUE INDEX `username` (`username` ASC))
+ENGINE = InnoDB
+AUTO_INCREMENT = 9
+DEFAULT CHARACTER SET = latin1;
 
-INSERT INTO Books(ISBN,title,genre,author,summary,datePublished,imagePath)
-VALUES ('0000000000000','A Sample Title','Fantasy','John Doe','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','2001-01-01','placeholder-2.png');
 
-INSERT INTO BookDetails(ISBN,bookCondition,location,availability)
-VALUES ('0000000000000','Good','Q1.2.1','Available');
+-- -----------------------------------------------------
+-- Table `lims`.`BookHistory`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `lims`.`BookHistory` (
+  `bookHistoryId` INT(11) NOT NULL AUTO_INCREMENT,
+  `userId` INT(11) NOT NULL,
+  `ISBN` CHAR(13) NOT NULL,
+  `dateCheckout` DATE NULL DEFAULT NULL,
+  `dateDue` DATE NULL DEFAULT NULL,
+  `dateReturned` DATE NULL DEFAULT NULL,
+  PRIMARY KEY (`bookHistoryId`),
+  INDEX `userId` (`userId` ASC),
+  INDEX `ISBN` (`ISBN` ASC),
+  CONSTRAINT `BookHistory_ibfk_1`
+    FOREIGN KEY (`userId`)
+    REFERENCES `lims`.`Users` (`userId`),
+  CONSTRAINT `BookHistory_ibfk_2`
+    FOREIGN KEY (`ISBN`)
+    REFERENCES `lims`.`Books` (`ISBN`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `lims`.`BookRequests`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `lims`.`BookRequests` (
+  `requestId` INT(11) NOT NULL AUTO_INCREMENT,
+  `userId` INT(11) NOT NULL,
+  `ISBN` CHAR(13) NOT NULL,
+  `dateRequested` DATE NOT NULL,
+  PRIMARY KEY (`requestId`),
+  INDEX `userId` (`userId` ASC),
+  INDEX `ISBN` (`ISBN` ASC),
+  CONSTRAINT `BookRequests_ibfk_1`
+    FOREIGN KEY (`userId`)
+    REFERENCES `lims`.`Users` (`userId`),
+  CONSTRAINT `BookRequests_ibfk_2`
+    FOREIGN KEY (`ISBN`)
+    REFERENCES `lims`.`Books` (`ISBN`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `lims`.`Orders`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `lims`.`Orders` (
+  `orderId` INT(11) NOT NULL AUTO_INCREMENT,
+  `ISBN` CHAR(13) NOT NULL,
+  `quantity` INT(11) NOT NULL,
+  `dateOrdered` DATE NOT NULL,
+  `dateExpected` DATE NULL DEFAULT NULL,
+  `dateRecieved` DATE NULL DEFAULT NULL,
+  PRIMARY KEY (`orderId`),
+  INDEX `ISBN` (`ISBN` ASC),
+  CONSTRAINT `Orders_ibfk_1`
+    FOREIGN KEY (`ISBN`)
+    REFERENCES `lims`.`Books` (`ISBN`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `lims`.`Reservations`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `lims`.`Reservations` (
+  `reservationId` INT(11) NOT NULL AUTO_INCREMENT,
+  `userId` INT(11) NOT NULL,
+  `bookId` INT(11) NULL DEFAULT NULL,
+  `dateReserved` DATE NOT NULL,
+  PRIMARY KEY (`reservationId`),
+  INDEX `userId` (`userId` ASC),
+  INDEX `bookId` (`bookId` ASC),
+  CONSTRAINT `Reservations_ibfk_1`
+    FOREIGN KEY (`userId`)
+    REFERENCES `lims`.`Users` (`userId`),
+  CONSTRAINT `Reservations_ibfk_2`
+    FOREIGN KEY (`bookId`)
+    REFERENCES `lims`.`BookDetails` (`bookId`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 5
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `lims`.`UserReviews`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `lims`.`UserReviews` (
+  `reviewId` INT(11) NOT NULL AUTO_INCREMENT,
+  `userId` INT(11) NOT NULL,
+  `ISBN` CHAR(13) NOT NULL,
+  `rating` INT(11) NOT NULL,
+  `reviewText` VARCHAR(2048) NULL DEFAULT NULL,
+  PRIMARY KEY (`reviewId`),
+  INDEX `userId` (`userId` ASC),
+  INDEX `ISBN` (`ISBN` ASC),
+  CONSTRAINT `UserReviews_ibfk_1`
+    FOREIGN KEY (`userId`)
+    REFERENCES `lims`.`Users` (`userId`),
+  CONSTRAINT `UserReviews_ibfk_2`
+    FOREIGN KEY (`ISBN`)
+    REFERENCES `lims`.`Books` (`ISBN`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
