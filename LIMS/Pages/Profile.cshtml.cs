b606d3d93f56ec8b5f66bcf47bf203cb6cab6ba0
Charles Noble <charlesddnoble@gmail.com>
2020-04-14 20:48:54 -0400

    Added profile information to profile page

--- LIMS/Pages/Profile.cshtml.cs
+++ LIMS/Pages/Profile.cshtml.cs
@@ -10,7 +10,14 @@ namespace LIMS
     public class ProfileModel : PageModel
     {
 
-
+        public string Username { get; private set;}
+        public string FirstName { get; private set; }
+        public string LastName { get; private set;}
+        public string Address { get; private set;}
+        public string City { get; private set;}
+        public string State { get; private set;}
+        public string Zip { get; private set;}
+        public string Phone { get; private set;}
         public class ProfileForm
         {
             public string Action { get; set; }
@@ -83,7 +90,36 @@ namespace LIMS
         
         public void OnGet()
         {
+            try
+            {
+                var handler = new ConnectionHandler();
+                using (MySqlConnection connection = handler.Connection)
+                {
+                    string sql = "SELECT username, firstName, lastName, address, city, state, zip, phone FROM Users WHERE userId=@USERID";
+                    MySqlCommand cmd = new MySqlCommand(sql, connection);
 
+                    cmd.Parameters.AddWithValue("@USERID", HttpContext.Session.GetString("userId"));
+
+                    using (MySqlDataReader rdr = cmd.ExecuteReader())
+                    {
+                        if (rdr.Read())
+                        {
+                            this.Username = rdr[0].ToString();
+                            this.FirstName = rdr[1].ToString();
+                            this.LastName = rdr[2].ToString();
+                            this.Address = rdr[3].ToString();
+                            this.City = rdr[4].ToString();
+                            this.State = rdr[5].ToString();
+                            this.Zip = rdr[6].ToString();
+                            this.Phone = rdr[7].ToString();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
     }
 }
